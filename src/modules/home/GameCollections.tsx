e1ace0346311be781ee854c1e6cf842373a4a97f
UdumiziSolomon <solomonudumizi@gmail.com>
2025-01-25 14:46:24 +0100

    feat: 🎸 added new btn

--- src/modules/home/GameCollections.tsx
+++ src/modules/home/GameCollections.tsx
@@ -5,7 +5,7 @@ import { Text } from "@/ui/primitives/typography";
 const GameCollections = () => {
   return (
     <Box className="my-[5rem] border-t-[1px] border-t-[#999] py-[1.5rem]">
-      <Text className="text-center font-medium text-[2rem]">
+      <Text className="text-center font-semibold text-[2rem] tracking-[.2px]">
         GAME COLLECTIONS
       </Text>
     </Box>
--- src/modules/home/HeroBox.tsx
+++ src/modules/home/HeroBox.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import Image from "next/image";
 import { Box, HStack } from "@/ui/primitives/ui-layout";
 import { Text } from "@/ui/primitives/typography";
+
+// icons
 import GameIcon from "@/svgs/game.svg";
 import CubeIcon from "@/svgs/cube.svg";
-
+import ApkIcon from "@/svgs/apk.svg";
 
 const HeroBox = () => {
   return (
@@ -21,9 +23,24 @@ const HeroBox = () => {
             Explore, Play <br />
             and Win Games
           </Text>
-          <HStack className="inline-flex my-[.5rem] bg-[#ffffff] text-center py-[.4rem] rounded-[10px] px-[2rem] gap-2">
-            <Image priority src={CubeIcon} alt="game" width={35} height={35} />
-            <Text className="text-[#000] text-[.9rem] pt-[3px]">Start Playing</Text>
+          <Text
+            className={
+              "font-regular text-[1rem] md:text-[1.2rem] ml-[1rem] leading-[30px]"
+            }
+          >
+            Discover popular games, fund your wallet and start winning...{" "}
+          </Text>
+          <HStack className="inline-flex my-[.5rem] bg-[#ffffff] text-center py-[.4rem] rounded-[10px] px-[2rem] gap-2 ml-[1rem]">
+            <Image priority src={CubeIcon} alt="game" width={32} height={32} />
+            <Text className="text-[#000] text-[.9rem] pt-[3px]">
+              Start Playing
+            </Text>
+          </HStack>
+          <HStack className="inline-flex my-[.5rem] border border-[#ffffff] text-center py-[.4rem] rounded-[10px] px-[2rem] gap-2 ml-[1rem]">
+            <Image priority src={ApkIcon} alt="apk" width={32} height={32} />
+            <Text className="text-[#ffffff] text-[.9rem] pt-[3px]">
+              Download APK
+            </Text>
           </HStack>
         </Box>
 
