8c8ba950045b6ed8f40b33c51ed46e55d3745e71
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2025-06-24 12:35:03 +0000

    .

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -22,6 +22,7 @@ const Page = () => {
     },
     onSuccess: (data) => {
       console.log(data)
+      router.push('/verify-otp')
     },
     onError: (error) => {
       console.error(error)
@@ -31,7 +32,6 @@ const Page = () => {
   
   const handleLogin = async (e: FormEvent) => {
     e.preventDefault()
-    // router.replace('/verify-otp')
     mutation.mutateAsync(email)
   }
 
@@ -84,31 +84,6 @@ const Page = () => {
               className="w-full pl-10 pr-4 pt-4 pb-3 bg-transparent border-[.2px] border-[#adb5bd] rounded-md text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 font-light text-[.9rem]"
             />
           </Box>
-
-          {/* Password Input */}
-          {/* <Box className="relative">
-            <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
-            <input
-              id="password"
-              name="password"
-              type={showPassword ? "text" : "password"}
-              autoComplete="current-password"
-              required
-              placeholder="Enter your password"
-              className="w-full pl-10 pr-10 pt-4 pb-3 bg-transparent border-[.2px] border-[#adb5bd] rounded-md text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 font-light text-[.9rem]"
-            />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-300 focus:outline-none"
-            >
-              {showPassword ? (
-                <EyeOff className="w-5 h-5" />
-              ) : (
-                <Eye className="w-5 h-5" />
-              )}
-            </button>
-          </Box> */}
 
           {/* Forgot Password Link */}
           <Box className="flex gap-2 items-center justify-end">
--- src/app/(auth)/verify-otp/page.tsx
+++ src/app/(auth)/verify-otp/page.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { Text } from "@/ui/primitives/typography";
+import { Box } from "@/ui/primitives/ui-layout";
+import Image from "next/image";
+import LogoIcon from "@/svgs/logo.svg";
 
 const VerifyOtp = () => {
   return (
-    <div>
-        Verify Otp
-    </div>
+    <Box
+      className="min-h-screen flex flex-col items-center justify-center"
+      style={{
+        background: `
+          radial-gradient(
+            ellipse at center,
+            rgba(92, 52, 156, 0.6) 15%,
+            rgba(17, 17, 29, 1) 85%
+          ),
+          linear-gradient(to bottom, #0a0a0a, #171717)
+        `,
+      }}
+    >
+        {/* Logo Container */}
+        <Image
+          priority
+          src={LogoIcon}
+          alt="logo"
+          width={90}
+          height={90}
+          className="mb-4"
+        />
+  
+        {/* Heading */}
+        <Text className="text-center font-medium text-[1.2rem] md:text-[1.5rem] text-white">
+          Sign in to your account
+        </Text>
+        <Text className="text-center font-light text-[.9rem] md:text-[.9rem] text-gray-300 mb-8">
+          Enter your email address below to proceed
+        </Text>
+
+         <Box className="w-full max-w-md px-4">
+          <p></p>
+         </Box>
+    </Box>
   )
 }
 
