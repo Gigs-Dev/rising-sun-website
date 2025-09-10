122f5c77d226e84bac25cfcc54190d6e1f03e104
Emmanuel Omejua <omejuaifeanyi2@mail.com>
2025-09-10 01:14:21 +0100

    winning logic!

--- src/modules/games/guess/Card.tsx
+++ src/modules/games/guess/Card.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useRef, useState } from 'react';
 import { shuffle } from './shuffle';
 
 
@@ -6,26 +8,69 @@ import { shuffle } from './shuffle';
 
 const items = [1, 2, 3, 4, 5, 6]
 
-const allItems = shuffle([...items, ...items])
+// const allItems = shuffle([...items, ...items])
 
-console.log(allItems);
+const defaultState = { index: -1, value: null }
 
 
 const Card = () => {
-  return (
-    <>
-        <div className='guess-card-container'>
-        {allItems.map((item, index) => {
-            return (
-                <div className="guess-cards" key={index}>
-                    <div className="backside"></div>
 
-                    <div className="">{item}</div>
-                </div>
-            )
-        })}
-        </div>
-    </>
+    const [allItems, setAllItems] = useState<number[]>([]);
+
+    const [firstCard, setFirstCard] = useState(defaultState);
+    const [secondCard, setSecondCard] = useState(defaultState);
+    const [remainingCard, setRemainingCard] = useState(items);
+
+
+    useEffect(() => {
+        setAllItems(shuffle([...items, ...items]))
+    }, [])
+
+    const timer = useRef<string | number | undefined | any>('')
+
+    const handleClick = (index: number, value: any) => {
+        clearTimeout(timer.current)
+
+        timer.current = setTimeout(() => {
+            setFirstCard(defaultState);
+            setSecondCard(defaultState);
+        }, 3000);
+
+
+        if(firstCard.index === -1 || (firstCard.index !== -1 && secondCard.index !== -1)){
+            setSecondCard(defaultState);
+            setFirstCard({index, value})
+        } else if(secondCard.index !== -1 && firstCard.index !== -1){
+            setSecondCard({index, value});
+
+            if(firstCard.value === value){
+                setRemainingCard(remainingCard.filter((card) => card !== value))
+            }
+        }
+    }
+
+
+
+  return (
+    <div className="guess-card-container">
+      {allItems.map((item, index) => {
+        const isFlipped =
+          firstCard.index === index ||
+          secondCard.index === index ||
+          !remainingCard.includes(index);
+
+        return (
+          <div
+            key={index}
+            onClick={() => handleClick(index, item)}
+            className={`guess-cards ${isFlipped ? 'flipped' : ''}`}
+          >
+            <div className="backside"></div>
+            {item}
+          </div>
+        );
+      })}
+    </div>
   )
 }
 
